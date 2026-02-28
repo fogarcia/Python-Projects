44d05add458b9891729cbfc62b9b1caf39832ec6
fogarcia <garciafoster403@gmail.com>
2026-02-28 12:44:48 -0500

    Added relative path structure and hashing

--- Small-Projects/file-integrity-checker/baseline.py
+++ Small-Projects/file-integrity-checker/baseline.py
@@ -1,3 +1,28 @@
+import hashlib
 from pathlib import Path
 
-print("Current directory:", Path.cwd())
+root = Path.cwd()
+chuck_size = 8192
+
+print(f"Root directory: {root}")
+
+def sha256(file_path) -> str:
+    h = hashlib.sha256()
+    with file_path.open("rb") as f:
+        while True:
+            chunk = f.read(chuck_size)
+            if not chunk:
+                break
+            h.update(chunk)
+    return h.hexdigest()
+
+count = 0
+
+for file in root.rglob("*"):
+    if file.is_file():
+        relative_path = file.relative_to(root)
+        digest = sha256(file)
+        print(f"{relative_path}: {digest}")
+        count += 1
+
+print(f"Total number of files: {count}")
