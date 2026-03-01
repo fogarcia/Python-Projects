538c566ab6767f31ad58471ddfdc868738101fe9
fogarcia <garciafoster403@gmail.com>
2026-03-01 14:42:40 -0500

    Finished reports.py and created/completed distances.py

--- Small-Projects/edX/report.py
+++ Small-Projects/edX/report.py
@@ -1,15 +1,21 @@
 def main():
+    # spacecraft = {
+    #     "name": "Voyager 1",
+    #     "distance": "163"
+    # }
     spacecraft = {
-        "name": "Voyager 1",
-        "distance": "163"
+        "name": "James Webb Space Telescope"
     }
+    spacecraft.update({"distance": "0.00001581", "orbit": "Sun"})
+    # spacecraft["distance"] = "0.00001581"
     print(create_report(spacecraft))
 
 def create_report(spacecraft):
     return f"""
     ============= Report =============
-    Name: {spacecraft["name"]}
-    Distance: {spacecraft["distance"]} AU
+    Name: {spacecraft.get("name", "Unknown")}
+    Distance: {spacecraft.get("distance", "Unknown")} AU
+    Orbit: {spacecraft.get("orbit", "Unknown")}
     
     ==================================
     """
