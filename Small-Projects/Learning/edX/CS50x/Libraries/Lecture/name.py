3e775a91b86a5ccde56029cd98e661dc1e7cd5e8
fogarcia <garciafoster403@gmail.com>
2026-05-09 14:24:08 -0400

    Added average.py and name.py to libraries lecture. CS50x.

---
+++ Small-Projects/Learning/edX/CS50x/Libraries/Lecture/name.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#     sys.exit("Too many arguments")
+# else:
+#     print("hello, my name is", sys.argv[1])
+
+# Print name tags
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
+
+# try:
+#     print("hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("Too few arguments")
