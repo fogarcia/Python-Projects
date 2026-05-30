6907cd1828c97237d3207b773503808791e719c8
fogarcia <garciafoster403@gmail.com>
2026-05-30 15:38:09 -0400

    Completed temp practice. MOOC.

---
+++ Small-Projects/Learning/MOOC/temperatures.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+fahrenheit = int(input("Please type in a temperature(F): "))
+
+celsius = (fahrenheit - 32) * 5/9
+
+if celsius < 0:
+    print(f'{fahrenheit} degrees Fahrenheit equals {celsius} degrees Celsius')
+    print("Brr! It's cold in here!")
+else:
+    print(f'{fahrenheit} degrees Fahrenheit equals {celsius} degrees Celsius')
