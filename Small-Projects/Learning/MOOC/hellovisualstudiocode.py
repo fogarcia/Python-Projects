d155be09aff885892902a08e14c657d4d90eb724
fogarcia <garciafoster403@gmail.com>
2026-06-27 13:34:50 -0400

    Started MOOC vs code section.

---
+++ Small-Projects/Learning/MOOC/hellovisualstudiocode.py
@@ -0,0 +1,12 @@
+while True:
+
+    editor = input("Editor:")
+
+    if editor.lower() == 'notepad' or editor.lower() == 'word':
+        print('awful')
+    elif editor.lower() == 'visual studio code':
+        print('an excellent choice!')
+        break
+    else:
+        print('not good')
+      
