6579af97df96f3daa9309e3a05052fa3c8aa4f50
fogarcia <garciafoster403@gmail.com>
2026-06-28 15:51:37 -0400

    Completed some exercises.

--- Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-09_greatest_number/src/greatest_number.py
+++ Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-09_greatest_number/src/greatest_number.py
@@ -1,4 +1,13 @@
 # Write your solution here
+def greatest_number(num, num1, num2):
+    if num >= num1 and num >= num2:
+        return num
+    elif num1 >= num and num1 >= num2:
+        return num1
+    elif num2 >= num and num2 >= num1:
+        return num2
+    else:
+        return num
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     greatest = greatest_number(5, 4, 8)
--- Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-10_same_characters/src/same_characters.py
+++ Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def same_chars(char, num, num1):
+    ...
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
