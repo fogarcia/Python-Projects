7bb579e94ab97140ba049ea947a6fac0fcd52925
fogarcia <garciafoster403@gmail.com>
2026-06-28 00:21:51 -0400

    a spruce

--- Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-08_spruce/src/spruce.py
+++ Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-08_spruce/src/spruce.py
@@ -1,12 +1,13 @@
 # Write your solution here 
 def spruce(num):
-
+    print('a spruce!')
     i = 0
 
     while i < num:
         if i < num:
-            print(f"{'*' * i}*{'*' * i}")
+            print(f"{' ' * (num - i - 1)}{'*' * i}*{'*' * i}")
             i += 1
+    print(f"{' ' * (num - 1)}*")
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    spruce(5)
+    spruce(3)
