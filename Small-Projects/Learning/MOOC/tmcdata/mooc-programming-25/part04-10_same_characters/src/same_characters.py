3f3e7f23d22e319d813e631469b9edb463d609d8
fogarcia <garciafoster403@gmail.com>
2026-06-28 18:14:21 -0400

    Added part 5 exercises.

--- Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-10_same_characters/src/same_characters.py
+++ Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-10_same_characters/src/same_characters.py
@@ -1,6 +1,11 @@
 # Write your solution here
-def same_chars(char, num, num1):
-    ...
+def same_chars(string, num, num1):
+    if num >= len(string) or num1 >= len(string):
+        return False
+    elif string[num] == string[num1]:
+        return True
+    else:
+        return False
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    print(same_chars("abc", 0, 3))
--- Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-11_first_second_last/src/first_second_last.py
+++ Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-11_first_second_last/src/first_second_last.py
@@ -1,7 +1,47 @@
 # Write your solution here
+def first_word(sentence):
+    index = 0
+
+    while index < len(sentence):
+
+        if sentence[index] == " ":
+            return sentence[0:index]
+        index+= 1
+
+def second_word(sentence):
+    index = 0
+
+    while index < len(sentence):
+
+        if sentence[index] == " ":
+
+            first_space = index
+
+            index = first_space + 1
+
+            while index < len(sentence):
+
+                if sentence[index] == " ":
+
+                    return sentence[first_space + 1:index]
+                
+                index += 1
+        index += 1
+    
+    return sentence[first_space + 1:]
+
+def last_word(sentence):
+    index = 0
+
+    while index < len(sentence):
+
+        if sentence[index] == " ":
+            return sentence[index + 1:]
+        index-= 1
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
     print(first_word(sentence))
-    print(second_word(sentence))
+    print(second_word("first second"))
     print(last_word(sentence))
