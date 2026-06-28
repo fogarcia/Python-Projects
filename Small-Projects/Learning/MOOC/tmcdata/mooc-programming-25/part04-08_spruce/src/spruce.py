eb2edf926e6ea91325b778c983705f57a0cedbfb
fogarcia <garciafoster403@gmail.com>
2026-06-27 23:04:27 -0400

    Making changes.

--- Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-08_spruce/src/spruce.py
+++ Small-Projects/Learning/MOOC/tmcdata/mooc-programming-25/part04-08_spruce/src/spruce.py
@@ -1,4 +1,12 @@
-# Write your solution here
+# Write your solution here 
+def spruce(num):
+
+    i = 0
+
+    while i < num:
+        if i < num:
+            print(f"{'*' * i}*{'*' * i}")
+            i += 1
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     spruce(5)
